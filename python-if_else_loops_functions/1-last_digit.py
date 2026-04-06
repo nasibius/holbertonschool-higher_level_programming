e4e1664e03efe998689e3b55b64a2ac5761e7231
Süleymanov Nəsib <72444222+nasibius@users.noreply.github.com>
2026-04-07 01:17:56 +0400

    tasks

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+num = number%10
+print(f"The digit of {number} is {num}", end=" ")
+if num>5:
+    print("and is greater than 5")
+elif num==0:
+    print("and is 0")
+elif num<6 and num!=0:
+    print("and is less than 6 and not 0")
+    
+      

63e03806acf85b2c2f320b8c98064b688025066f
Süleymanov Nəsib <72444222+nasibius@users.noreply.github.com>
2026-04-09 20:06:07 +0400

    tasks

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+
+    max_val = my_list[0]
+    for n in my_list:
+        if n > max_val:
+            max_val = n
+    return max_val

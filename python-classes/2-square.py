98db7a26760227333110a3d645615a33f0e5f363
Süleymanov Nəsib <72444222+nasibius@users.noreply.github.com>
2026-04-10 15:46:26 +0400

    tasks

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,6 +5,7 @@
 class Square:
     """a square"""
     def __init__(self, size=0):
+        """init"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
